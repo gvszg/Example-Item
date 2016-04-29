62866aa3f90501ea87fb552a2fd6a84a7aa26888
gvszg <zetaentranceserious@gmail.com>
2016-04-29 21:49:02 +0800

    Refactor SQL query to scope

--- app/controllers/admin/sales_reports_controller.rb
+++ app/controllers/admin/sales_reports_controller.rb
@@ -4,7 +4,9 @@ class Admin::SalesReportsController < AdminController
   def item_sales_result
     if %w[month week day].include?(params[:time_field])
     # if params[:time_field] == "month" || params[:time_field] == "week" || params[:time_field] == "day"
-      @item_sales = OrderItem.includes(:item, item: :categories).group(:source_item_id).select(:id, :item_name, :source_item_id, "SUM(item_quantity) as sum_item_quantity").where(created_at: which_time_field_params).order("sum_item_quantity DESC")
+      # @item_sales = OrderItem.includes(:item, item: :categories).group(:source_item_id).select(:id, :item_name, :source_item_id, "SUM(item_quantity) as sum_item_quantity").where(created_at: which_time_field_params).order("sum_item_quantity DESC")
+
+      @item_sales = OrderItem.sort_by_item_sales(params[:time_field])
     else
       @item_sales = OrderItem.includes(:item, item: :categories).group(:source_item_id).select(:id, :item_name, :source_item_id, "SUM(item_quantity) as sum_item_quantity").order("sum_item_quantity DESC")
     end
@@ -25,13 +27,13 @@ class Admin::SalesReportsController < AdminController
 
   def item_revenue_result
     if %w[month week day].include?(params[:time_field])
-      @item_revenue = OrderItem.includes(:item, item: :categories).group(:source_item_id).select(:id, :item_name, :source_item_id, "SUM(item_quantity * item_price) as sum_item_revenue").where(created_at: which_time_field_params).order("sum_item_revenue DESC")
+      @item_revenue = OrderItem.sort_by_item_revenue(params[:time_field])
     else
       @item_revenue = OrderItem.includes(:item, item: :categories).group(:source_item_id).select(:id, :item_name, :source_item_id, "SUM(item_quantity * item_price) as sum_item_revenue").order("sum_item_revenue DESC")
     end
   end
 
-  private
+  # private
 
   # def intial_item_sales
   #   @item_sales = OrderItem.includes(:item, item: :categories).group(:source_item_id).select(:id, :item_name, :source_item_id, "SUM(item_quantity) as sum_item_quantity").order("sum_item_quantity DESC")
@@ -41,14 +43,14 @@ class Admin::SalesReportsController < AdminController
   #   @item_sales = OrderItem.includes(:item, item: :categories).group(:source_item_id).select(:id, :item_name, :source_item_id, "SUM(item_quantity) as sum_item_quantity").where(created_at: which_time_field_params).order("sum_item_quantity DESC")
   # end
 
-  def which_time_field_params
-    case params[:time_field]
-    when "month"
-      return (Time.now - 30.day)..Time.now
-    when "week"
-      return (Time.now - 7.day)..Time.now
-    when "day"
-      return (Time.now - 1.day)..Time.now
-    end
-  end
+  # def which_time_field_params
+  #   case params[:time_field]
+  #   when "month"
+  #     return (Time.now - 30.day)..Time.now
+  #   when "week"
+  #     return (Time.now - 7.day)..Time.now
+  #   when "day"
+  #     return (Time.now - 1.day)..Time.now
+  #   end
+  # end
 end
